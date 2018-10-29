e2976db280fc2303a4a401d91e8c94bb913b792e
zjislong <zjislong@users.noreply.github.com>
2018-10-29 19:53:56 +0800

    fix error

--- module.erl
+++ module.erl
@@ -1,4 +1,3 @@
-%%%'   HEADER
 %%% @author {{author_name}} <{{author_email}}>
 %%% @copyright {{copyright_year}} {{author_name}}
 %%% @doc gen_server callback module implementation:
@@ -9,14 +8,10 @@
 
 -export([]).
 
-%%%.
-%%%'   PUBLIC API
+%%====================================================================
+%% API functions
+%%====================================================================
 
-% TODO: add more public API here...
-
-%%%.
-%%%'   PRIVATE FUNCTIONS
-
-
-%%%.
-%%% vim: set filetype=erlang tabstop=2 foldmarker=%%%',%%%. foldmethod=marker:
+%%====================================================================
+%% Internal functions
+%%====================================================================
